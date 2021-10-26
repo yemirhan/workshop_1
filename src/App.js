05cf2c49cfd55ec523c7a16c120468a3aca108eb
Yusuf Emirhan <syusufemirhan@gmail.com>
2021-10-26 21:00:11 +0300

    Styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { Home } from './containers/Home'
 function App() {
   return (
-    <Home />
+    <div style={{ display: "flex", position: "fixed", width: "100%", height: "100%", bottom: 0, left: 0, right: 0, top: 0, alignItems: "center", justifyContent: "center" }}>
+      <Home />
+    </div>
   );
 }
 
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
-
+import "./Home.css"
 export const Home = () => {
     const [data, setdata] = useState(null)
     const fetchData = async () => await axios.get("https://api.nasa.gov/planetary/apod?api_key=2gxas4LusuRoMS86UYUUONRCYxMeVqXMoKtY3RL5")
@@ -9,9 +9,23 @@ export const Home = () => {
     }, [])
     if (!data) return <div>loading...</div>
     return (
-        <div>
+        <div className="card">
             {data.explanation}
             {/* {data?.explanation || "loading..."} ES6 */}
+            {/* <style>
+                {
+                    `
+                    .card {
+                        width: 300px;
+                        height: auto;
+                        border: 1px solid #ccc;
+                        border-radius: 12px;
+                        box-shadow: 1px 1px 1px 1px #ddd;
+                        padding: 10px;
+                    }
+                    `
+                }
+            </style> */}
         </div>
     )
 }
