8c8adbc98713288df45db1b8219d46ac8cca3982
Yusuf Emirhan <syusufemirhan@gmail.com>
2021-10-26 20:29:53 +0300

    Input setstate

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
 import { Button, ButtonDestructured } from './Button'
+import { Input } from './Input'
 
 export const Home = () => {
-    const [state, setstate] = useState(0)
+    // const [state, setstate] = useState(0)
+    const [value, setValue] = useState("")
+    const onChange = (e) => setValue(e.target.value);
+    const onChangeDestructured = ({ target: { value } }) => setValue(value);
     return (
         <div>
-            <Button s={setstate} />
+            {/* <Button s={setstate} />
             {state}
-            <ButtonDestructured setstate={setstate} state={state} />
+            <ButtonDestructured setstate={setstate} state={state} /> */}
+            <Input value={value} onChange={onChangeDestructured} />
         </div>
     )
 }
